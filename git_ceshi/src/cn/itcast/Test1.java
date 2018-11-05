8f2c121d85f881501985a685008fe6fbb21aa28a
zhouli <zhouli@163.com>
2018-11-05 19:43:59 +0800

    将java2工程上传到git

---
+++ git_ceshi/src/cn/itcast/Test1.java
@@ -0,0 +1,9 @@
+package cn.itcast;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("git,要git");
+        System.out.println("git,要git");
+        System.out.println("git,要git");
+    }
+}
